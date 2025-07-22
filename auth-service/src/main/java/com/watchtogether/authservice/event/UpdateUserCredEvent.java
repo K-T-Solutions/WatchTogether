80c9fa5ab0cf1e07c511949fefebdacd897977e5
Antonchyk Denis <tenirey11@gmail.com>
2025-07-22 12:05:01 +0300

    feat(auth): introduce UpdateUserCredEvent for user credential updates
    - Create a new event class to handle user credential updates
    - Add properties for user ID, new login, and credential type
    - Use Lombok for easier construction and getters/setters

---
+++ auth-service/src/main/java/com/watchtogether/authservice/event/UpdateUserCredEvent.java
@@ -0,0 +1,16 @@
+package com.watchtogether.authservice.event;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Builder
+@Getter
+@Setter
+public class UpdateUserCredEvent {
+    private UUID userId;
+    private String newLogin; //TODO: подумать как назвать
+    private String credType;
+}
