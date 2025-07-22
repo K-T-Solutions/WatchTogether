1dbd208b78df343b440fcc2250a8b1cbab3d592f
Antonchyk Denis <tenirey11@gmail.com>
2025-07-22 12:05:56 +0300

    refactor(kafka): introduce UpdateUserCredEvent, update send methods

--- auth-service/src/main/java/com/watchtogether/authservice/kafka/KafkaProducer.java
+++ auth-service/src/main/java/com/watchtogether/authservice/kafka/KafkaProducer.java
@@ -1,7 +1,7 @@
 package com.watchtogether.authservice.kafka;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.watchtogether.authservice.entity.AuthCredentialsEntity;
+import com.watchtogether.authservice.event.UpdateUserCredEvent;
 import com.watchtogether.authservice.event.UserRegisteredEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,14 +24,37 @@ public class KafkaProducer {
             kafkaTemplate.send("user-registered", event.getUserId().toString(), eventBytes)
                     .whenComplete((result, ex) -> {
                         if (ex == null) {
-                            log.info("Event sent successfully for user with id: {}", event.getUserId());
+                            log.info("Register event sent successfully for user with id: {}", event.getUserId());
                         } else {
-                            log.error("Failed to send event for user with id: {}", event.getUserId(), ex);
+                            log.error("Failed to send register event for user with id: {}", event.getUserId(), ex);
                         }
                     });
 
         } catch (Exception e) {
-            log.error("Error serializing event for user with id: {}", event.getUserId(), e);
+            log.error("Error serializing register event for user with id: {}", event.getUserId(), e);
         }
     }
+
+    public void sendUpdateUserCredEvent(UpdateUserCredEvent event) {
+
+        try {
+            byte[] eventBytes = objectMapper.writeValueAsBytes(event);
+
+            log.info("Sending update user cred event for user with id: {}", event.getUserId());
+
+            kafkaTemplate.send("update-user-cred", event.getUserId().toString(), eventBytes)
+                    .whenComplete((result, ex) -> {
+                        if (ex == null) {
+                            log.info("Update cred event sent successfully for user with id: {}", event.getUserId());
+                        } else {
+                            log.error("Failed to send update cred event for user with id: {}", event.getUserId(), ex);
+                        }
+                    });
+
+        } catch (Exception e) {
+            log.error("Error serializing update cred event for user with id: {}", event.getUserId(), e);
+        }
+
+    }
+
 }
