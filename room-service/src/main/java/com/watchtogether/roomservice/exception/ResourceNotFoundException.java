b54a23da1aaf41e1a5e19a3737109b4ea36b54ba
Antonchyk Denis <tenirey11@gmail.com>
2025-07-22 10:40:16 +0300

    feat(exception): introduce new exceptions for RoomNotFoundException, PermissionDeniedException, ResourceNotFoundException, InvalidTokenException and NotRoomMemberException
    - Create three new exception classes for handling specific cases in the RoomService application.
    - Use RuntimeException as the base class for all new exceptions.

---
+++ room-service/src/main/java/com/watchtogether/roomservice/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.watchtogether.roomservice.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ room-service/src/main/java/com/watchtogether/roomservice/exception/RoomNotFoundException.java
@@ -0,0 +1,7 @@
+package com.watchtogether.roomservice.exception;
+
+public class RoomNotFoundException extends RuntimeException {
+    public RoomNotFoundException(String message) {
+        super(message);
+    }
+}
