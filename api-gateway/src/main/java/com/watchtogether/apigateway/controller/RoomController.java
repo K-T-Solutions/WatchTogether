28cff3def7387cdd569944e77159189cd7315c98
Antonchyk Denis <tenirey11@gmail.com>
2025-08-01 19:38:04 +0300

    feat(api-gateway/controller): introduce RoomController for managing room operations
    - Add RoomController class to handle GraphQL queries and mutations for rooms
    - Implement methods for getting a room by ID, getting all rooms, and creating a new room
    - Create dependencies for Grpc clients and Lombok
    
    Note: The implementation for getting rooms by category is currently a TODO item.

---
+++ api-gateway/src/main/java/com/watchtogether/apigateway/controller/RoomController.java
@@ -0,0 +1,42 @@
+package com.watchtogether.apigateway.controller;
+
+import com.watchtogether.apigateway.grpc.RoomGrpcClient;
+import com.watchtogether.grpc.CreateRoomRequestGrpc;
+import com.watchtogether.grpc.RoomCategoryGrpc;
+import com.watchtogether.grpc.RoomListResponseGrpc;
+import com.watchtogether.grpc.RoomResponseGrpc;
+import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.UUID;
+
+@Controller
+@RequiredArgsConstructor
+public class RoomController {
+    private final RoomGrpcClient roomGrpcClient;
+
+    @QueryMapping
+    public RoomResponseGrpc getRoomById(@Argument UUID roomId) {
+        return roomGrpcClient.getRoomById(roomId.toString());
+    }
+
+    @QueryMapping
+    public RoomListResponseGrpc getAllRooms() {
+        return roomGrpcClient.getAllRooms();
+    }
+
+    @QueryMapping
+    public RoomListResponseGrpc getRoomsByCategory(@Argument RoomCategoryGrpc category) {
+        return null; //TODO: impl
+    }
+
+    @MutationMapping
+    public RoomResponseGrpc createRoom(@Argument CreateRoomRequestGrpc request) {
+        return roomGrpcClient.createRoom(request);
+    }
+
+
+}
