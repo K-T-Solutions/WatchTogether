48e261fd2eb0a978c13a3b2110fba3c3572ee4cb
Antonchyk Denis <tenirey11@gmail.com>
2025-08-02 11:04:12 +0300

    refactored project structure

---
+++ services/notification-service/src/main/java/com/watchtogether/notificationservice/kafka/KafkaConsumer.java
@@ -0,0 +1,16 @@
+package com.watchtogether.notificationservice.kafka;
+
+import com.watchtogether.notificationservice.event.MessageEvent;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    public void handleMessageNotification(MessageEvent event) {
+
+
+
+    }
+
+
+}
