3b1acc552a3267a3fd3a517aaa08387b281639e3
Antonchyk Denis <tenirey11@gmail.com>
2025-08-09 00:55:14 +0300

    fix(auth-service): handle various Grpc exceptions
    - Add exception handlers for IllegalArgumentException, BadCredentialsException, UsernameNotFoundException, EmailAlreadyVerifiedException, and Exception.
    - Implement proper status codes and error messages for each exception.

--- services/auth-service/src/main/java/com/watchtogether/authservice/exception/GlobalGrpcExceptionHandler.java
+++ services/auth-service/src/main/java/com/watchtogether/authservice/exception/GlobalGrpcExceptionHandler.java
@@ -3,6 +3,8 @@ package com.watchtogether.authservice.exception;
 import io.grpc.Status;
 import net.devh.boot.grpc.server.advice.GrpcAdvice;
 import net.devh.boot.grpc.server.advice.GrpcExceptionHandler;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @GrpcAdvice
 public class GlobalGrpcExceptionHandler {
@@ -25,6 +27,31 @@ public class GlobalGrpcExceptionHandler {
     @GrpcExceptionHandler(InvalidCredentialsException.class)
     public Status handleInvalidCredentialsException(InvalidCredentialsException e) {
         return Status.ABORTED.withDescription(e.getMessage()).withCause(e); //TODO: change status
+    }
+
+    @GrpcExceptionHandler(IllegalArgumentException.class)
+    public Status handleIllegalArgumentException(IllegalArgumentException e) {
+        return Status.INVALID_ARGUMENT.withDescription(e.getMessage()).withCause(e);
+    }
+
+    @GrpcExceptionHandler(BadCredentialsException.class)
+    public Status handleBadCredentials(BadCredentialsException e) {
+        return Status.INVALID_ARGUMENT.withDescription(e.getMessage()).withCause(e);
+    }
+
+    @GrpcExceptionHandler(UsernameNotFoundException.class)
+    public Status handleUsernameNotFound(UsernameNotFoundException e) {
+        return Status.NOT_FOUND.withDescription(e.getMessage()).withCause(e);
+    }
+
+    @GrpcExceptionHandler(EmailAlreadyVerifiedException.class)
+    public Status handleEmailAlreadyVerified(EmailAlreadyVerifiedException e) {
+        return Status.FAILED_PRECONDITION.withDescription(e.getMessage()).withCause(e);
+    }
+
+    @GrpcExceptionHandler(Exception.class)
+    public Status handleGeneric(Exception e) {
+        return Status.INTERNAL.withDescription("Internal error").withCause(e);
     }
 
 }
