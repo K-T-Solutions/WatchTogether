2020815ebe1642adedead112de9dd366ca606822
Antonchyk Denis <tenirey11@gmail.com>
2025-08-08 19:26:41 +0300

    refactor(security/config, service/TwoFactorAuth): Simplify mail configuration and MailSender implementation
    - Remove the @Configuration annotation from MailConfig and MailSender classes
    - Remove the @RequiredArgsConstructor and @Service annotations from MailSender
    - Eliminate the use of @Value injection for properties in both classes
    - Simplify the implementation of MailSender by removing the setFrom field and using the provided JavaMailSender instance

--- services/auth-service/src/main/java/com/watchtogether/authservice/security/config/MailConfig.java
+++ services/auth-service/src/main/java/com/watchtogether/authservice/security/config/MailConfig.java
@@ -8,57 +8,57 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
-//@Configuration
-public class MailConfig { //TODO: change here
-//    @Value("${spring.mail.host}")
-//    private String host;
-//
-//    @Value("${spring.mail.username}")
-//    private String username;
-//
-//    @Value("${spring.mail.password}")
-//    private String password;
-//
-//    @Value("${spring.mail.port}")
-//    private int port;
-//
-//    @Value("${spring.mail.protocol}")
-//    private String protocol;
-//
-//    @Value("${spring.mail.properties.mail.debug:true}")
-//    private String debug;
-//
-//    @Bean
-//    public JavaMailSender getMailSender() {
-//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-//
-//        mailSender.setHost(host);
-//        mailSender.setPort(port);
-//        mailSender.setUsername(username);
-//        mailSender.setPassword(password);
-//
-//        Properties properties = mailSender.getJavaMailProperties();
-//
-//        // Основные настройки протокола
-//        properties.setProperty("mail.transport.protocol", protocol);
-//        properties.setProperty("mail.debug", debug);
-//
-//        // Настройки аутентификации
-//        properties.setProperty("mail.smtp.auth", "true");
-//
-//        // Настройки STARTTLS
-//        properties.setProperty("mail.smtp.starttls.enable", "true");
-//        properties.setProperty("mail.smtp.starttls.required", "true");
-//
-//        // Настройки SSL/TLS
-//        properties.setProperty("mail.smtp.ssl.protocols", "TLSv1.2");
-//        properties.setProperty("mail.smtp.ssl.trust", host);
-//
-//        // Таймауты
-//        properties.setProperty("mail.smtp.connectiontimeout", "5000");
-//        properties.setProperty("mail.smtp.timeout", "5000");
-//        properties.setProperty("mail.smtp.writetimeout", "5000");
-//
-//        return mailSender;
-//    }
+@Configuration
+public class MailConfig {
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.protocol}")
+    private String protocol;
+
+    @Value("${spring.mail.properties.mail.debug:true}")
+    private String debug;
+
+    @Bean
+    public JavaMailSender getMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties properties = mailSender.getJavaMailProperties();
+
+        // Основные настройки протокола
+        properties.setProperty("mail.transport.protocol", protocol);
+        properties.setProperty("mail.debug", debug);
+
+        // Настройки аутентификации
+        properties.setProperty("mail.smtp.auth", "true");
+
+        // Настройки STARTTLS
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.smtp.starttls.required", "true");
+
+        // Настройки SSL/TLS
+        properties.setProperty("mail.smtp.ssl.protocols", "TLSv1.2");
+        properties.setProperty("mail.smtp.ssl.trust", host);
+
+        // Таймауты
+        properties.setProperty("mail.smtp.connectiontimeout", "5000");
+        properties.setProperty("mail.smtp.timeout", "5000");
+        properties.setProperty("mail.smtp.writetimeout", "5000");
+
+        return mailSender;
+    }
 }
