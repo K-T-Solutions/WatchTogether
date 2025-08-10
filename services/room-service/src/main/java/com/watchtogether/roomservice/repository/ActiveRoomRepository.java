845399bfcace59e566c5c19ce3b9d27482393756
Antonchyk Denis <tenirey11@gmail.com>
2025-08-10 22:29:27 +0300

    feat(application): introduce Redis and caching support
    - Add @EnableCaching and @EnableRedisRepositories annotations
    - Remove unnecessary @EnableWebMvc annotation
    - Update dependencies to include Spring Boot Starter Data Redis

---
+++ services/room-service/src/main/java/com/watchtogether/roomservice/repository/ActiveRoomRepository.java
@@ -0,0 +1,19 @@
+package com.watchtogether.roomservice.repository;
+
+import com.watchtogether.grpc.RoomCategory;
+import com.watchtogether.roomservice.entity.ActiveRoomEntity;
+import com.watchtogether.roomservice.enums.RoomType;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ActiveRoomRepository extends CrudRepository<ActiveRoomEntity, String> {
+
+    Optional<ActiveRoomEntity> findByOwnerId(String ownerId);
+
+    List<ActiveRoomEntity> findAllByType(RoomType type);
+
+    List<ActiveRoomEntity> findAllByCategory(RoomCategory category);
+
+}
