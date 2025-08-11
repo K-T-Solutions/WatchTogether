2f43d17c3c4db673d2f17d9fd5bd03e365c5775c
Antonchyk Denis <tenirey11@gmail.com>
2025-08-11 18:06:51 +0300

    feat(api-gateway/graphql/controller): add RoomResponse and CreateRoomResponse DTOs, update mutation and query mappings
    - Add RoomResponse and CreateRoomResponse DTOs to encapsulate response data
    - Update query and mutation mappings to use new DTOs
    - Refactor RoomController class to use new DTOs in response methods
    
    Breaking changes: The RoomResponse and CreateRoomResponse types have changed.

--- services/api-gateway/src/main/java/com/watchtogether/apigateway/graphql/controller/RoomController.java
+++ services/api-gateway/src/main/java/com/watchtogether/apigateway/graphql/controller/RoomController.java
@@ -1,44 +1,110 @@
 package com.watchtogether.apigateway.graphql.controller;
 
+import com.watchtogether.apigateway.dto.CreateRoomDto;
+import com.watchtogether.apigateway.enums.GraphQLRoomCategory;
+import com.watchtogether.apigateway.enums.GraphQLRoomType;
 import com.watchtogether.apigateway.grpc.RoomGrpcClient;
-import com.watchtogether.grpc.CreateRoomRequestGrpc;
-import com.watchtogether.grpc.RoomCategoryGrpc;
-import com.watchtogether.grpc.RoomListResponseGrpc;
-import com.watchtogether.grpc.RoomResponseGrpc;
+import com.watchtogether.apigateway.util.RoomMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:5173")
 public class RoomController {
     private final RoomGrpcClient roomGrpcClient;
+    private final RoomMapper roomMapper;
 
     @QueryMapping
-    public RoomResponseGrpc getRoomById(@Argument UUID roomId) {
-        return roomGrpcClient.getRoomById(roomId.toString());
+    public RoomResponse getRoomById(@Argument UUID roomId) {
+        var grpcResponse = roomGrpcClient.getRoomById(roomId.toString());
+        return new RoomResponse(
+                grpcResponse.getRoomId(),
+                grpcResponse.getOwnerId(),
+                grpcResponse.getRoomName(),
+                grpcResponse.getRoomDescription(),
+                roomMapper.mapTypeToGraphQL(grpcResponse.getRoomType()),
+                roomMapper.mapCategoryToGraphQL(grpcResponse.getRoomCategory()),
+                grpcResponse.getMaxParticipants(),
+                grpcResponse.getNeedPassword());
     }
 
     @QueryMapping
-    public RoomListResponseGrpc getAllRooms() {
-        return roomGrpcClient.getAllRooms();
+    public RoomResponse getRoomByOwnerId(@Argument UUID ownerId) {
+        var grpcResponse = roomGrpcClient.getRoomByOwnerId(ownerId.toString());
+        return new RoomResponse(
+                grpcResponse.getRoomId(),
+                grpcResponse.getOwnerId(),
+                grpcResponse.getRoomName(),
+                grpcResponse.getRoomDescription(),
+                roomMapper.mapTypeToGraphQL(grpcResponse.getRoomType()),
+                roomMapper.mapCategoryToGraphQL(grpcResponse.getRoomCategory()),
+                grpcResponse.getMaxParticipants(),
+                grpcResponse.getNeedPassword());
     }
 
     @QueryMapping
-    public RoomListResponseGrpc getRoomsByCategory(@Argument RoomCategoryGrpc category) {
-        return null; //TODO: impl
+    public List<RoomResponse> getAllPublicRooms() {
+        var grpcResponse = roomGrpcClient.getAllPublicRooms();
+        return grpcResponse.getRoomsList()
+                .stream()
+                .map(room -> new RoomResponse(
+                        room.getRoomId(),
+                        room.getOwnerId(),
+                        room.getRoomName(),
+                        room.getRoomDescription(),
+                        roomMapper.mapTypeToGraphQL(room.getRoomType()),
+                        roomMapper.mapCategoryToGraphQL(room.getRoomCategory()),
+                        room.getMaxParticipants(),
+                        room.getNeedPassword())).toList();
     }
 
     @MutationMapping
-    public RoomResponseGrpc createRoom(@Argument CreateRoomRequestGrpc request) {
-        return roomGrpcClient.createRoom(request);
+    public CreateRoomResponse createRoom(@Argument CreateRoomDto request) {
+        var grpcResponse = roomGrpcClient.createRoom(request);
+
+
+        return new CreateRoomResponse(
+                grpcResponse.getSuccess(),
+                grpcResponse.getMessage());
     }
 
+    @MutationMapping //TODO: change
+    public boolean addParticipantToRoom(@Argument UUID roomId,
+                                        @Argument UUID participantId,
+                                        @Argument String password
+    ) {
+        return roomGrpcClient.addParticipant(roomId.toString(), participantId.toString(), password).getSuccess();
+    }
+
+    @MutationMapping
+    public boolean removeParticipantFromRoom(@Argument UUID roomId,
+                                        @Argument UUID participantId
+    ) {
+        return roomGrpcClient.removeParticipant(roomId.toString(), participantId.toString())
+                .getSuccess();
+    }
+
+    public record RoomResponse(
+                        String roomId,
+                        String ownerId,
+                        String roomName,
+                        String roomDescription,
+                        GraphQLRoomType roomType,
+                        GraphQLRoomCategory roomCategory,
+                        int maxParticipants,
+                        boolean needPassword) {}
+
+    public record CreateRoomResponse(Boolean success,
+                                     String message) {}
 
 }
