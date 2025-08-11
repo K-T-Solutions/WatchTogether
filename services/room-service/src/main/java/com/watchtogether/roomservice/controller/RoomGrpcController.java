175a64f6838282eb9051942a1902362ecc6dfc06
Antonchyk Denis <tenirey11@gmail.com>
2025-08-11 18:04:06 +0300

    feat(grpc-controller): add support for getting all public rooms`
       - Introduce a method to get all public rooms and implement it using the RoomService.
       - Remove the getAllRoomsByCategory method as it's no longer needed.

--- services/room-service/src/main/java/com/watchtogether/roomservice/controller/RoomGrpcController.java
+++ services/room-service/src/main/java/com/watchtogether/roomservice/controller/RoomGrpcController.java
@@ -1,9 +1,10 @@
 package com.watchtogether.roomservice.controller;
 
-import com.watchtogether.roomservice.entity.RoomEntity;
+import com.google.protobuf.Empty;
+import com.watchtogether.roomservice.entity.ActiveRoomEntity;
 import com.watchtogether.roomservice.service.room.IRoomService;
-import com.watchtogether.roomservice.util.RoomGrpcMapper;
 import com.watchtogether.grpc.*;
+import io.grpc.ServerInterceptor;
 import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,8 +12,6 @@ import net.devh.boot.grpc.server.service.GrpcService;
 //import org.springframework.security.core.context.SecurityContextHolder; // Пример получения пользователя
 
 import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,73 +21,104 @@ public class RoomGrpcController extends com.watchtogether.grpc.RoomServiceGrpc.R
     private final IRoomService roomService;
 
     @Override
-    public void getRoomById(
-            RoomIdRequestGrpc request,
-            StreamObserver<RoomResponseGrpc> responseObserver
-    ) {
-        log.info("gRPC getRoomById, id: {}", request.getRoomId());
-        var room = roomService.getRoomById(UUID.fromString(request.getRoomId()));
-        responseObserver.onNext(RoomGrpcMapper.toRoomResponseGrpc(room));
+    public void getRoomById(GetRoomByIdRequest request, StreamObserver<RoomResponse> responseObserver) {
+        var roomEntity = roomService.findRoomById(request.getRoomId());
+        responseObserver.onNext(RoomResponse.newBuilder()
+                        .setRoomId(roomEntity.getId())
+                        .setOwnerId(roomEntity.getOwnerId())
+                        .setRoomName(roomEntity.getName())
+                        .setRoomDescription(roomEntity.getDescription())
+                        .setRoomType(roomEntity.getType())
+                        .setRoomCategory(roomEntity.getCategory())
+                        .setMaxParticipants(roomEntity.getMaxParticipants())
+                        .setNeedPassword(!roomEntity.getPasswordHash().isEmpty())
+                        .build());
         responseObserver.onCompleted();
     }
 
     @Override
-    public void getAllRooms(
-            com.watchtogether.grpc.EmptyGrpc request,
-            StreamObserver<RoomListResponseGrpc> responseObserver
-    ) {
-        log.info("gRPC getAllRooms");
-        var rooms = roomService.getAllRooms();
-        var roomProtos = rooms.stream()
-                .map(RoomGrpcMapper::toRoomGrpc)
-                .collect(Collectors.toList());
-        responseObserver.onNext(RoomListResponseGrpc.newBuilder().addAllRooms(roomProtos).build());
+    public void getRoomByOwnerId(GetRoomByOwnerIdRequest request, StreamObserver<RoomResponse> responseObserver) {
+        super.getRoomByOwnerId(request, responseObserver);
+    }
+
+    @Override
+    public void getAllPublicRooms(Empty request, StreamObserver<RoomListResponse> responseObserver) {
+
+        List<ActiveRoomEntity> publicRooms = roomService.findAllPublicRooms();
+
+        log.info("Rooms name: {}", publicRooms.getFirst().getName());
+
+        responseObserver.onNext(RoomListResponse.newBuilder()
+                        .addAllRooms(publicRooms
+                                .stream()
+                                .map(this::mapToGrpcRoomResponse)
+                                .toList())
+                        .build());
         responseObserver.onCompleted();
     }
 
     @Override
-    public void getRoomsByCategory(
-            CategoryRequestGrpc request,
-            StreamObserver<RoomListResponseGrpc> responseObserver
-    ) {
-        log.info("gRPC getRoomsByCategory: {}", request.getCategory());
-        List<RoomEntity> rooms = roomService.getAllRoomsByCategory(request.getCategory());
-        var roomProtos = rooms.stream()
-                .map(RoomGrpcMapper::toRoomGrpc)
-                .collect(Collectors.toList());
-        responseObserver.onNext(RoomListResponseGrpc.newBuilder().addAllRooms(roomProtos).build());
+    public void createRoom(CreateRoomRequest request, StreamObserver<CreateRoomResponse> responseObserver) {
+
+
+        var createdRoom = roomService.createRoom(request); //TODO: add exception handle
+        responseObserver.onNext(CreateRoomResponse.newBuilder()
+                        .setSuccess(true)
+                        .setMessage("Room created successfully")
+                        .build());
         responseObserver.onCompleted();
     }
 
     @Override
-    public void createRoom(
-            CreateRoomRequestGrpc request,
-            StreamObserver<RoomResponseGrpc> responseObserver
-    ) {
-        log.info("gRPC createRoom: {}", request.getName());
-        // ID пользователя берется из безопасного контекста, а не из запроса!
-        String userId = UUID.randomUUID().toString(); //TODO:CHANGE Заглушка, здесь должна быть логика
+    public void addParticipantToRoom(AddParticipantRequest request, StreamObserver<AddParticipantResponse> responseObserver) { //TODO: ну тут как то надо все же проверять
 
-        var roomEntity = roomService
-                .createRoom(RoomGrpcMapper
-                        .fromCreateRequestGrpc(request), UUID.fromString(userId));
+        boolean success;
 
-        responseObserver.onNext(RoomGrpcMapper.toRoomResponseGrpc(roomEntity));
+        if (request.getPassword().isEmpty()) {
+            success = roomService.addParticipantToRoom(request.getRoomId(), request.getParticipantId());
+            log.info("two par");
+            log.info("pass: {}", request.getPassword());
+        } else {
+            log.info("three par");
+            success = roomService.addParticipantToRoom(request.getRoomId(), request.getParticipantId(), request.getPassword());
+        }
+
+        responseObserver.onNext(AddParticipantResponse.newBuilder()
+                        .setSuccess(success)
+                        .setMessage(success ? "Participant added successfully" : "Participant not added")
+                        .build());
         responseObserver.onCompleted();
     }
 
     @Override
-    public void updateRoom(UpdateRoomRequestGrpc request, StreamObserver<RoomResponseGrpc> responseObserver) {
-        log.info("gRPC updateRoom: {}", request.getRoomId());
-        String userId = "извлечь_из_grpc_metadata";
-
-        // Здесь ваш сервис должен принимать DTO для обновления
-        var updatedEntity = roomService.updateRoom(
-                RoomGrpcMapper.fromUpdateRequest(request),
-                UUID.fromString(request.getRoomId()),
-                UUID.fromString(userId));//TODO: refactor
-
-        responseObserver.onNext(RoomGrpcMapper.toRoomResponseGrpc(updatedEntity));
+    public void removeParticipantFromRoom(RemoveParticipantRequest request, StreamObserver<RemoveParticipantResponse> responseObserver) {
+        boolean success = roomService.removeParticipantFromRoom(request.getRoomId(), request.getParticipantId()); //TODO: ловить искл.
+        responseObserver.onNext(RemoveParticipantResponse.newBuilder()
+                .setSuccess(success)
+                .setMessage(success ? "Participant removed successfully" : "Participant not removed")
+                .build());
         responseObserver.onCompleted();
     }
+
+    private RoomResponse mapToGrpcRoomResponse(ActiveRoomEntity entity) {
+        return RoomResponse.newBuilder()
+                .setRoomId(entity.getId())
+                .setOwnerId(entity.getOwnerId())
+                .setRoomName(entity.getName())
+                .setRoomDescription(entity.getDescription() != null ? entity.getDescription() : "")
+                .setRoomType(entity.getType())
+                .setRoomCategory(entity.getCategory())
+                .setMaxParticipants(entity.getMaxParticipants())
+                .setNeedPassword(!entity.getPasswordHash().isEmpty())
+//                .addAllParticipantIds(entity.getParticipantIds()) // если есть список участников
+//                .setCreatedAt(Timestamp.newBuilder()
+//                        .setSeconds(entity.getCreatedAt().getEpochSecond())
+//                        .setNanos(entity.getCreatedAt().getNano())
+//                        .build())
+//                .setStatus(mapStatusToGrpc(entity.getStatus()))
+                .build();
+    }
+
+
+
 }
