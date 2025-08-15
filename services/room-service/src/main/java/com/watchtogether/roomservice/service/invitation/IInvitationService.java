7eff60bbce6ae25cc55947912a670dcd16e8ba18
Antonchyk Denis <tenirey11@gmail.com>
2025-08-16 00:38:54 +0300

    feat(room-service/invitation): simplify date formatting logic in InvitationService
    - Extracted date formatting logic into a separate method for easier maintenance.
    - Utilized java.time API for better consistency and readability.

--- services/room-service/src/main/java/com/watchtogether/roomservice/service/invitation/IInvitationService.java
+++ services/room-service/src/main/java/com/watchtogether/roomservice/service/invitation/IInvitationService.java
@@ -2,7 +2,6 @@ package com.watchtogether.roomservice.service.invitation;
 
 
 import com.watchtogether.grpc.GenerateInvitationRequest;
-import com.watchtogether.grpc.JoinRoomByInviteRequest;
 import com.watchtogether.grpc.JoinToRoomResponse;
 import com.watchtogether.roomservice.entity.InvitationEntity;
 
@@ -15,5 +14,4 @@ public interface IInvitationService {
 
     String generateInvitation(GenerateInvitationRequest request);
 
-    JoinToRoomResponse.Builder joinRoomByInvite(JoinRoomByInviteRequest grpcRequest);
 }
